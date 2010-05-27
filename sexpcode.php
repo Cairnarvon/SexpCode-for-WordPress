460b582f0848f91ddd3edc7046532fbf3d078ee4
Cairnarvon <cairnarvon@gmail.com>
2010-05-27 23:40:50 +0200

    Initial commit
    Support for basic SexpCode tags without arguments, plus escaping. Only
    {} tags are supported.

---
+++ sexpcode.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Plugin Name: SexpCode for WordPress
+Plugin URI: http://cairnarvon.rotahall.org/2010/05/25/towards-a-better-bbcode/
+Description: Enables the use of SexpCode in comments.
+Version: 1.0
+Author: Koen Crolla
+Author URI: http://cairnarvon.rotahall.org/
+*/
+
+function sexpcode_translate($input)
+{
+    $tags = array('b' => array('open'  => '<b>',
+                               'close' => '</b>',
+                               'iter'  => false),
+                  'i' => array('open'  => '<i>',
+                               'close' => '</i>',
+                               'iter'  => false),
+                  'u' => array('open'  => '<u>',
+                               'close' => '</u>',
+                               'iter'  => false),
+                  's' => array('open'  => '<s>',
+                               'close' => '</s>',
+                               'iter'  => false),
+                  'o' => array('open'  => '<span style="text-decoration: ' .
+                                          'overline">',
+                               'close' => '</span>',
+                               'iter'  => false),
+                  'sub' => array('open'  => '<sub>',
+                               'close' => '</sub>',
+                               'iter'  => true),
+                  'sup' => array('open'  => '<sup>',
+                                 'close' => '</sup>',
+                                 'iter'  => true),
+                  'code' => array('open'  => '<code>',
+                                  'close' => '</code>',
+                                  'iter'  => false),
+                  'spoiler' => array('open'  => '<span style="background:' .
+                                                ' #000" onmouseover="this' .
+                                                '.style.color=\'#FFF\';" ' .
+                                                'onmouseout="this.style.c' .
+                                                'olor=this.style.backgrou' .
+                                                'ndColor=\'#000\'">',
+                                     'close' => '</span>',
+                                     'iter'  => false),
+                  'quote' => array('open'  => '<blockquote>',
+                                   'close' => '</blockquote>',
+                                   'iter'  => true),
+                  'blockquote' => array('open'  => '<blockquote>',
+                                        'close' => '</blockquote>',
+                                        'iter'  => true),
+                  'm' => array('open'  => '<pre>',
+                               'close' => '</pre>',
+                               'iter'  => false));
+
+    $input = strtr($input, array('\\' => '&#92;',
+                                 '\\{' => '&#123;',
+                                 '\\}' => '&#125;'));
+
+    $closers = array();
+    $i = $j = $k = 0;
+
+    $out = "";
+
+    while ($i < strlen($input)) {
+        if ($input[$i] == '{') {
+            if (($j = strpos($input, ' ', $i)) === false) return input;
+            $expr = substr($input, $i + 1, $j - $i - 1);
+
+            $open = "";
+            $close = "";
+
+            $subs = explode('.', $expr);
+            foreach ($subs as $sub) {
+                list($tag, $n) = explode('*', $sub, 2);
+
+                if ($tags[$tag] !== null) {
+                    $n = $n === null || !$tags[$tag]['iter'] ? 1 : $n + 0;
+                    if ($n > 3) $n = 3;
+
+                    while ($n-->0) {
+                        $open .= $tags[$tag]['open'];
+                        $close = $tags[$tag]['close'] . $close;
+                    }
+                } else return $input;
+            }
+
+            $out .= substr($input, $k, $i - $k) . $open;
+            $closers[] = $close;
+            $k = $j + 1;
+
+        } elseif ($input[$i] == '}') {
+            if (($j = count($closers)) < 1) return $input;
+            --$j;
+
+            $out .= substr($input, $k, $i - $k) . $closers[$j];
+            unset($closers[$j]);
+            $closers = array_values($closers);
+
+            $k = $i + 1;
+
+        }
+        ++$i;
+    }
+
+    $out .= substr($input, $k);
+
+    while (($i = count($closers)) > 0) {
+        --$i;
+        $out .= $closers[$i];
+        unset($closers[$i]);
+        $closers = array_values($closers);
+    }
+
+    return $out;
+}
+
+add_filter('comment_text', 'sexpcode_translate', 5);
+
+?>
